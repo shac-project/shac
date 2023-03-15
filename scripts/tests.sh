fedbd3270937d731637cb59b7768b55498a04a0d
Oliver Newman <olivernewman@google.com>
2023-03-15 20:07:16 +0000

    Add placeholder tests.sh script
    Change-Id: Id14b3f38099cfcd566ba63106d435c7f93b3d3c0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/shac-project/shac/+/820047
    Fuchsia-Auto-Submit: Oliver Newman <olivernewman@google.com>
    Reviewed-by: Marc-Antoine Ruel <maruel@google.com>
    Commit-Queue: Oliver Newman <olivernewman@google.com>

---
+++ scripts/tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Copyright 2023 The Fuchsia Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+set -eu -o pipefail
+
+# TODO(olivernewman): Actually run tests once there are tests to run.
+echo "No tests run."
