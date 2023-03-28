d79c975a082e07390eea44ca0f96fc6276ff9f98
Marc-Antoine Ruel <maruel@google.com>
2023-03-28 17:03:01 +0000

    [engine] Move more unit tests to table driven
    Increase coverage to close to 94%.
    
    Test only change.
    
    Change-Id: Ib295d2c61f96030658539feb6d728457a4593e5e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/shac-project/shac/+/824561
    Reviewed-by: Oliver Newman <olivernewman@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Marc-Antoine Ruel <maruel@google.com>

--- internal/engine/load_test.go
+++ internal/engine/load_test.go
@@ -20,163 +20,6 @@ import (
 	"go.chromium.org/luci/common/errors"
 )
 
-func TestLoad_Backtrace(t *testing.T) {
-	err := Load(context.Background(), "testdata", "backtrace.star")
-	if err == nil {
-		t.Fatal("expected a failure")
-	}
-	if s := err.Error(); s != "inner" {
-		t.Fatal(s)
-	}
-	var errs errors.MultiError
-	if !errors.As(err, &errs) {
-		t.Fatal("not a MultiError")
-	}
-	if len(errs) != 1 {
-		t.Fatal("expected one wrapped error")
-	}
-	var err2 BacktracableError
-	if !errors.As(errs[0], &err2) {
-		t.Fatal("not a backtracable error")
-	}
-	want := `Traceback (most recent call last):
-  //backtrace.star:11:4: in <toplevel>
-  //backtrace.star:9:6: in fn1
-  //backtrace.star:6:7: in fn2
-  <builtin>: in fail
-Error: inner`
-	if diff := cmp.Diff(want, err2.Backtrace()); diff != "" {
-		t.Fatalf("mismatch (+want -got):\n%s", diff)
-	}
-}
-
-func TestLoad_Dir_Native(t *testing.T) {
-	b := getErrPrint(t)
-	if err := Load(context.Background(), "testdata", "dir_native.star"); err != nil {
-		t.Fatal(err)
-	}
-	if s := b.String(); s != "[//dir_native.star:5] [\"commitHash\", \"version\"]\n" {
-		t.Fatal(s)
-	}
-}
-
-func TestLoad_Dir_Shac(t *testing.T) {
-	b := getErrPrint(t)
-	if err := Load(context.Background(), "testdata", "dir_shac.star"); err != nil {
-		t.Fatal(err)
-	}
-	if s := b.String(); s != "[//dir_shac.star:6] [\"exec\", \"io\", \"re\", \"result\", \"scm\"]\n" {
-		t.Fatal(s)
-	}
-}
-
-func TestLoad_IO_Read_File(t *testing.T) {
-	b := getErrPrint(t)
-	if err := Load(context.Background(), "testdata", "io_read_file.star"); err != nil {
-		t.Fatal(err)
-	}
-	if s := b.String(); s != "[//io_read_file.star:7] {\"key\": \"value\"}\n" {
-		t.Fatal(s)
-	}
-}
-
-func TestLoad_Minimal(t *testing.T) {
-	b := getErrPrint(t)
-	if err := Load(context.Background(), "testdata", "minimal.star"); err != nil {
-		t.Fatal(err)
-	}
-	v := fmt.Sprintf("(%d, %d, %d)", version[0], version[1], version[2])
-	if s := b.String(); s != "[//minimal.star:5] "+v+"\n" {
-		t.Fatal(s)
-	}
-}
-
-func TestLoad_Re_All_Matches(t *testing.T) {
-	b := getErrPrint(t)
-	if err := Load(context.Background(), "testdata", "re_allmatches.star"); err != nil {
-		t.Fatal(err)
-	}
-	want := `[//re_allmatches.star:7] ()
-[//re_allmatches.star:9] (match(groups = ("TODO(foo)",), offset = 4), match(groups = ("TODO(bar)",), offset = 14))
-[//re_allmatches.star:11] (match(groups = ("anc", "n", "c"), offset = 0),)
-`
-	if s := b.String(); s != want {
-		t.Fatal(s)
-	}
-}
-
-func TestLoad_Re_Match(t *testing.T) {
-	b := getErrPrint(t)
-	if err := Load(context.Background(), "testdata", "re_match.star"); err != nil {
-		t.Fatal(err)
-	}
-	want := `[//re_match.star:7] None
-[//re_match.star:9] match(groups = ("TODO(foo)",), offset = 4)
-[//re_match.star:11] match(groups = ("anc", "n", "c"), offset = 0)
-`
-	if s := b.String(); s != want {
-		t.Fatal(s)
-	}
-}
-
-func TestLoad_Register_Check(t *testing.T) {
-	b := getErrPrint(t)
-	if err := Load(context.Background(), "testdata", "register_check.star"); err != nil {
-		t.Fatal(err)
-	}
-	if s := b.String(); s != "[//register_check.star:6] running\n" {
-		t.Fatal(s)
-	}
-}
-
-func TestLoad_Register_Check_Recursive(t *testing.T) {
-	if err := Load(context.Background(), "testdata", "register_check_recursive.star"); err == nil {
-		t.Fatal("expected error")
-	} else if s := err.Error(); s != "can't register checks after done loading" {
-		t.Fatal(s)
-	}
-}
-
-func makeGit(t *testing.T) string {
-	root := t.TempDir()
-	runGit(t, root, "init")
-	writeFile(t, root, "file1.txt", "First file")
-	runGit(t, root, "add", "file1.txt")
-	runGit(t, root, "commit", "-m", "Initial commit")
-	// TODO(maruel): scm.go requires two commits. Not really worth fixing yet,
-	// it's only annoying in unit tests.
-	writeFile(t, root, "file2.txt", "First file")
-	runGit(t, root, "add", "file2.txt")
-	runGit(t, root, "commit", "-m", "Second commit")
-	return root
-}
-
-func copyFile(t *testing.T, dst, path string) {
-	d, err := os.ReadFile(filepath.Join("testdata", path))
-	if err != nil {
-		t.Fatal(err)
-	}
-	writeFile(t, dst, path, string(d))
-}
-
-func writeFile(t *testing.T, root, path, content string) {
-	if err := os.WriteFile(filepath.Join(root, path), []byte(content), 0o600); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func runGit(t *testing.T, root string, args ...string) string {
-	cmd := exec.Command("git", args...)
-	cmd.Dir = root
-	// First is for git version before 2.32, the rest are to skip the user and system config.
-	cmd.Env = append(os.Environ(), "GIT_CONFIG_NOGLOBAL=true", "GIT_CONFIG_GLOBAL=", "GIT_CONFIG_SYSTEM=", "EMAIL=test@example.com", "GIT_AUTHOR_DATE=2000-01-01T00:00:00", "GIT_COMMITTER_DATE=2000-01-01T00:00:00")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		t.Fatal(err)
-	}
-	return strings.TrimSpace(string(out))
-}
-
 func TestLoad_SCM_Affected_Files_Raw(t *testing.T) {
 	root := t.TempDir()
 	writeFile(t, root, "file1.txt", "First file")
@@ -262,95 +105,130 @@ func TestLoad_SCM_All_Files_Git(t *testing.T) {
 
 // TestTestDataFail runs all the files under testdata/fail/.
 func TestTestDataFail(t *testing.T) {
-	p := filepath.Join("testdata", "fail")
-	d, err := os.ReadDir(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-	got := make([]string, len(d))
-	for i := range d {
-		if !d[i].IsDir() {
-			got[i] = d[i].Name()
-		}
-	}
+	t.Parallel()
+	p, got := enumDir(t, "fail")
 	inexistant, err := filepath.Abs(filepath.Join("testdata", "fail", "inexistant"))
 	if err != nil {
 		t.Fatal(err)
 	}
+	// TODO(maruel): Fix the error to include the call site when applicable.
 	data := []struct {
-		name string
-		err  error
+		name  string
+		err   string
+		trace string
 	}{
 		{
+			"backtrace.star",
+			"inner",
+			`  //backtrace.star:11:4: in <toplevel>
+  //backtrace.star:9:6: in fn1
+  //backtrace.star:6:7: in fn2
+  <builtin>: in fail
+Error: inner`,
+		},
+		{
 			"empty.star",
-			errors.New("did you forget to call register_check?"),
+			"did you forget to call register_check?",
+			"",
 		},
 		{
 			"fail.star",
-			errors.New("an expected failure"),
+			"an expected failure",
+			`  //fail.star:5:5: in <toplevel>
+  <builtin>: in fail
+Error: an expected failure`,
 		},
 		{
 			"io_read_file_abs.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("do not use absolute path"),
+			"do not use absolute path",
+			"",
 		},
 		{
 			"io_read_file_escape.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("cannot escape root"),
+			"cannot escape root",
+			"",
 		},
 		{
 			"io_read_file_inexistant.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("open " + inexistant + ": no such file or directory"),
+			"open " + inexistant + ": no such file or directory",
+			"",
 		},
 		{
 			"io_read_file_missing_arg.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("read_file: got 0 arguments, want 1"),
+			"read_file: got 0 arguments, want 1",
+			"",
 		},
 		{
 			"io_read_file_unclean.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("pass cleaned path"),
+			"pass cleaned path",
+			"",
 		},
 		{
 			"io_read_file_windows.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("use POSIX style path"),
+			"use POSIX style path",
+			"",
 		},
 		{
 			"re_allmatches_no_arg.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("allmatches: missing argument for pattern"),
+			"allmatches: missing argument for pattern",
+			"",
 		},
 		{
 			"re_match_bad_re.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("error parsing regexp: missing closing ): `(`"),
+			"error parsing regexp: missing closing ): `(`",
+			"",
 		},
 		{
 			"re_match_no_arg.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("match: missing argument for pattern"),
+			"match: missing argument for pattern",
+			"",
 		},
 		{
 			"register_check_kwargs.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("register_check: unexpected keyword arguments"),
+			"register_check: unexpected keyword arguments",
+			`  //register_check_kwargs.star:8:15: in <toplevel>
+Error in register_check: register_check: unexpected keyword arguments`,
 		},
 		{
 			"register_check_no_arg.star",
-			// TODO(maruel): Fix the error to include the call site.
-			errors.New("register_check: got 0 arguments, want 1"),
+			"register_check: got 0 arguments, want 1",
+			`  //register_check_no_arg.star:5:15: in <toplevel>
+Error in register_check: register_check: got 0 arguments, want 1`,
+		},
+		{
+			"register_check_recursive.star",
+			"can't register checks after done loading",
+			"",
+		},
+		{
+			"scm_affected_files_arg.star",
+			"affected_files: unexpected arguments",
+			"",
+		},
+		{
+			"scm_affected_files_kwarg.star",
+			"affected_files: unexpected keyword arguments",
+			"",
+		},
+		{
+			"scm_all_files_arg.star",
+			"all_files: unexpected arguments",
+			"",
+		},
+		{
+			"scm_all_files_kwarg.star",
+			"all_files: unexpected keyword arguments",
+			"",
 		},
 		{
 			"syntax_error.star",
-			errors.New("//syntax_error.star:5:3: got '//', want primary expression"),
+			"//syntax_error.star:5:3: got '//', want primary expression",
+			"",
 		},
 		{
 			"undefined_symbol.star",
-			errors.New("//undefined_symbol.star:5:1: undefined: undefined_symbol"),
+			"//undefined_symbol.star:5:1: undefined: undefined_symbol",
+			"",
 		},
 	}
 	want := make([]string, len(data))
@@ -361,10 +239,28 @@ func TestTestDataFail(t *testing.T) {
 		t.Fatalf("mismatch (+want -got):\n%s", diff)
 	}
 	for i := range data {
+		i := i
 		t.Run(data[i].name, func(t *testing.T) {
+			t.Parallel()
 			err := Load(context.Background(), p, data[i].name)
-			if !equalError(data[i].err, err) {
-				if diff := cmp.Diff(data[i].err, err); diff != "" {
+			if err == nil {
+				t.Fatal("expecting an error")
+			}
+			if diff := cmp.Diff(data[i].err, err.Error()); diff != "" {
+				t.Fatalf("mismatch (+want -got):\n%s", diff)
+			}
+			err = unwrapMultiError(t, err)
+			expectTrace := data[i].trace != ""
+			var err2 BacktracableError
+			if errors.As(err, &err2) != expectTrace {
+				if expectTrace {
+					t.Fatal("expected backtracable error")
+				} else {
+					t.Fatalf("unexpected backtracable error: %s", err2.Backtrace())
+				}
+			}
+			if expectTrace {
+				if diff := cmp.Diff("Traceback (most recent call last):\n"+data[i].trace, err2.Backtrace()); diff != "" {
 					t.Fatalf("mismatch (+want -got):\n%s", diff)
 				}
 			}
@@ -372,8 +268,138 @@ func TestTestDataFail(t *testing.T) {
 	}
 }
 
-func equalError(a, b error) bool {
-	return a == nil && b == nil || a != nil && b != nil && a.Error() == b.Error()
+// TestTestDataSimple runs all the files under testdata/simple/.
+func TestTestDataSimple(t *testing.T) {
+	p, got := enumDir(t, "simple")
+	v := fmt.Sprintf("(%d, %d, %d)", version[0], version[1], version[2])
+	data := []struct {
+		name string
+		want string
+	}{
+		{
+			"dir_native.star",
+			"[//dir_native.star:5] [\"commitHash\", \"version\"]\n",
+		},
+		{
+			"dir_shac.star",
+			"[//dir_shac.star:6] [\"exec\", \"io\", \"re\", \"result\", \"scm\"]\n",
+		},
+		{
+			"io_read_file.star",
+			"[//io_read_file.star:7] {\"key\": \"value\"}\n",
+		},
+		{
+			"minimal.star",
+			"[//minimal.star:5] " + v + "\n",
+		},
+		{
+			"re_allmatches.star",
+			`[//re_allmatches.star:7] ()
+[//re_allmatches.star:9] (match(groups = ("TODO(foo)",), offset = 4), match(groups = ("TODO(bar)",), offset = 14))
+[//re_allmatches.star:11] (match(groups = ("anc", "n", "c"), offset = 0),)
+`,
+		},
+		{
+			"re_match.star",
+			`[//re_match.star:7] None
+[//re_match.star:9] match(groups = ("TODO(foo)",), offset = 4)
+[//re_match.star:11] match(groups = ("anc", "n", "c"), offset = 0)
+`,
+		},
+		{
+			"register_check.star",
+			"[//register_check.star:6] running\n",
+		},
+	}
+	want := make([]string, len(data))
+	for i := range data {
+		want[i] = data[i].name
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("mismatch (+want -got):\n%s", diff)
+	}
+	for i := range data {
+		i := i
+		t.Run(data[i].name, func(t *testing.T) {
+			b := getErrPrint(t)
+			if err := Load(context.Background(), p, data[i].name); err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(data[i].want, b.String()); diff != "" {
+				t.Fatalf("mismatch (+want -got):\n%s", diff)
+			}
+		})
+	}
+}
+
+// Utilities
+
+func unwrapMultiError(t *testing.T, err error) error {
+	// TODO(maruel): Use go 1.20 unwrap.
+	var errs errors.MultiError
+	if !errors.As(err, &errs) {
+		return err
+	}
+	if len(errs) != 1 {
+		t.Fatal("expected one wrapped error")
+	}
+	return errs[0]
+}
+
+func enumDir(t *testing.T, name string) (string, []string) {
+	p := filepath.Join("testdata", name)
+	d, err := os.ReadDir(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out := make([]string, 0, len(d))
+	for i := range d {
+		n := d[i].Name()
+		if strings.HasSuffix(n, ".star") && !d[i].IsDir() {
+			out = append(out, n)
+		}
+	}
+	return p, out
+}
+
+func makeGit(t *testing.T) string {
+	root := t.TempDir()
+	runGit(t, root, "init")
+	writeFile(t, root, "file1.txt", "First file")
+	runGit(t, root, "add", "file1.txt")
+	runGit(t, root, "commit", "-m", "Initial commit")
+	// TODO(maruel): scm.go requires two commits. Not really worth fixing yet,
+	// it's only annoying in unit tests.
+	writeFile(t, root, "file2.txt", "First file")
+	runGit(t, root, "add", "file2.txt")
+	runGit(t, root, "commit", "-m", "Second commit")
+	return root
+}
+
+func copyFile(t *testing.T, dst, path string) {
+	d, err := os.ReadFile(filepath.Join("testdata", path))
+	if err != nil {
+		t.Fatal(err)
+	}
+	writeFile(t, dst, path, string(d))
+}
+
+func writeFile(t *testing.T, root, path, content string) {
+	if err := os.WriteFile(filepath.Join(root, path), []byte(content), 0o600); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func runGit(t *testing.T, root string, args ...string) string {
+	cmd := exec.Command("git", args...)
+	cmd.Dir = root
+	// First is for git version before 2.32, the rest are to skip the user and system config.
+	cmd.Env = append(os.Environ(), "GIT_CONFIG_NOGLOBAL=true", "GIT_CONFIG_GLOBAL=", "GIT_CONFIG_SYSTEM=", "EMAIL=test@example.com", "GIT_AUTHOR_DATE=2000-01-01T00:00:00", "GIT_COMMITTER_DATE=2000-01-01T00:00:00")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return strings.TrimSpace(string(out))
 }
 
 func getErrPrint(t *testing.T) *bytes.Buffer {
